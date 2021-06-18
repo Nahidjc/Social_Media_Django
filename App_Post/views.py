69a326aa41bb269088c29bedda0fa80fe3207a4b
Nahidjc <nahidjc80@gmail.com>
2021-06-18 14:27:15 +0600

    search function add

--- App_Post/views.py
+++ App_Post/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 # Create your views here.
 
 
 @login_required
 def home(request):
-    return render(request, 'App_Post/home.html', context={'title': 'homepage'})
+    if request.method == 'GET':
+        search = request.GET.get('search')
+        result = User.objects.filter(username=search)
+    return render(request, 'App_Post/home.html', context={'title': 'homepage', 'search': search, })
